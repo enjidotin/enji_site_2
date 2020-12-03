cbda1aa94b32abdab6efd8479b36dcbe4bb00312
Naman Garg <naman.garg.naman@gmail.com>
2020-12-03 22:49:47 +0530

    try

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -17,6 +17,7 @@ export default () => (
           <AboutMe />
           <Skills />
           <ProjectCard/>
+          <video src="https://moodio-videos.s3.us-east-2.amazonaws.com/0001.mp4">oops</video>
         </>
       </SidebarWrapper>
     </Layout>
